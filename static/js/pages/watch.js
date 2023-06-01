bccb89cc1307de46175af7d1f2ce7776d85b580d
ThullyDeveloper <121730449+thullyDev@users.noreply.github.com>
2023-06-02 01:54:57 +0200

    Update watch.js

--- static/js/pages/watch.js
+++ static/js/pages/watch.js
@@ -86,7 +86,8 @@ $(() => {
   const render_anime_details = (data, watch_type = "") => {
 	if (data.episodesList.length == 0) window.location.replace("/alert?message=The%20first%20episode%20hasn%27t%20not%20come%20out%20yet&sub_message=please%20wait%20for%20it%20to%20be%20air");
     let genres_text = "";
-    anime_title = data.animeTitle;
+    const anime_title = data.animeTitle;
+    g_anime_tile = data.animeTitle;
     episode_list = data.episodesList.reverse();
     let index = episode_num - 1;
     episode = watch_type == "" ? episode_list[index] : episode_list[0];
